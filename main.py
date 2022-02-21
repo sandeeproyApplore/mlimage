01b50403a7d3498ab7ea2f40d3445e27bf7de9bf
sandeeproyApplore <sandeep.roy@applore.in>
2022-02-21 13:43:19 +0530

    dedug

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import cv2
 import os
 
 app = Flask(__name__)
-picFolder = os.path.join('uploads','piclab')
-app.config['UPLOAD_FOLDER'] = picFolder
+uploadFolder = os.getcwd() +'/uploads'
+app.config['UPLOAD_FOLDER'] = uploadFolder
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 def allowed_file(filename):
@@ -23,16 +23,15 @@ def backgroundchange():
             for file in files:
                 if file and allowed_file(file.filename):
                     filename = secure_filename(file.filename)
-                    file.save(filename)
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
             change_bg = alter_bg()
             change_bg.load_pascalvoc_model("deeplabv3_xception_tf_dim_ordering_tf_kernels.h5")
-            output = change_bg.change_bg_img(f_image_path = files[0].filename  ,b_image_path = files[1].filename )
-            cv2.imwrite("updatedimg.jpg", output)
-            resImg = cv2.imread('updatedimg.jpg')
-            os.unlink(files[0].filename)
-            os.unlink(files[1].filename)
-            return jsonify({"msg":"Successfully Uploaded!!"})  
+            output = change_bg.change_bg_img(f_image_path = os.path.join(app.config['UPLOAD_FOLDER'], files[0].filename)  ,b_image_path = os.path.join(app.config['UPLOAD_FOLDER'], files[1].filename))
+            cv2.imwrite(os.path.join(app.config['UPLOAD_FOLDER'], "updatedimg.jpg"), output)
+            os.unlink(os.path.join(app.config['UPLOAD_FOLDER'] ,files[0].filename))
+            os.unlink(os.path.join(app.config['UPLOAD_FOLDER'] ,files[1].filename))
+            return send_file('updatedimg.jpg', mimetype='image/gif') 
    
     else:
     #    pic1 = cv2.imread('uploads/piclab/updatedimg.jpg')
@@ -44,4 +43,6 @@ def backgroundchange():
 
 
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
     app.run(host='0.0.0.0',port=8080)    
